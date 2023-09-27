c018d0e97577e692219088602414b6c1574b0864
Pavel Alimov <pashaalimov@gmail.com>
2023-09-27 12:57:02 +0300

    Update activation tests

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,25 +1,38 @@
 import numpy as np
 import pytest
 
-from degann.networks.activations import perceptron_threshold
+from degann.networks.activations import perceptron_threshold, parabolic
 from tests.utils import array_compare
 
 
 @pytest.mark.parametrize(
     "threshold, inp, expected",
     [
-        (1, np.array([10], dtype=float), np.array([1], dtype=float)),
-        (1, np.array([1], dtype=float), np.array([1], dtype=float)),
-        (1, np.array([-10], dtype=float), np.array([0], dtype=float)),
+        (2, np.array([10], dtype=float), np.array([1], dtype=float)),
+        (2, np.array([1], dtype=float), np.array([0], dtype=float)),
+        (2, np.array([-10], dtype=float), np.array([0], dtype=float)),
         (0, np.array([10], dtype=float), np.array([1], dtype=float)),
         (0, np.array([1], dtype=float), np.array([1], dtype=float)),
         (0, np.array([-10], dtype=float), np.array([0], dtype=float)),
     ],
 )
-def test_create_layer(threshold, inp, expected):
+def test_perceptron_activation(threshold, inp, expected):
     act_func = perceptron_threshold
-    assert array_compare(act_func(inp), expected)
+    assert array_compare(act_func(inp, threshold), expected)
 
 
-def test_my():
-    assert True
+@pytest.mark.parametrize(
+    "inp, beta, p, expected",
+    [
+        (np.array([10], dtype=float), 0, 1 / 5, np.array([2], dtype=float)),
+        (np.array([1], dtype=float), 0, 1 / 50, np.array([0.2], dtype=float)),
+        (np.array([-10], dtype=float), 0, 1 / 5, np.array([-2], dtype=float)),
+        (np.array([10], dtype=float), 1, 1 / 5, np.array([3], dtype=float)),
+        (np.array([1], dtype=float), 1, 2, np.array([3], dtype=float)),
+        (np.array([-10], dtype=float), -3, 5, np.array([-13], dtype=float)),
+        (np.array([0], dtype=float), 42, 1 / 5, np.array([42], dtype=float)),
+    ],
+)
+def test_parabolic_activation(inp, beta, p, expected):
+    act_func = parabolic
+    assert array_compare(act_func(inp, beta, p), expected)
