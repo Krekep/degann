01a987e3d08be08bf40cc54f15c170c6cbaf0267
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2024-05-27 16:08:53 +0300

    Update to tensorflow 2.16

--- degann/networks/imodel.py
+++ degann/networks/imodel.py
@@ -491,29 +491,6 @@ class IModel(object):
 
         return res
 
-    # perceptron activation is discrete and has no derivatives (gradient)
-    # @classmethod
-    # def create_perceptron(cls, input_size, output_size, shape, threshold=1, **kwargs):
-    #     activation, decorator_params, weight, biases, kwargs = _get_act_and_init(
-    #         kwargs,
-    #         activations.perceptron_threshold,
-    #         [{"threshold": threshold}],
-    #         tf.random_normal_initializer(),
-    #     )
-    #
-    #     res = cls(
-    #         input_size=input_size,
-    #         block_size=shape,
-    #         output_size=output_size,
-    #         activation_func=activation,
-    #         bias_init=biases,
-    #         weight_init=weight,
-    #         decorator_params=decorator_params,
-    #         **kwargs,
-    #     )
-    #
-    #     return res
-
 
 _create_functions = defaultdict(lambda: TensorflowDenseNet)
 _create_functions["DenseNet"] = TensorflowDenseNet
