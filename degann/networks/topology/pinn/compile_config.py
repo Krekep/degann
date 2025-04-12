65eeb12ddf082af444920c7430f70942b316d6a6
Daniil Kostromitsky <DaniilK004@mail.ru>
2025-04-12 18:51:12 +0300

    feat(pinn/compile_config): add pinn Compile params

---
+++ degann/networks/topology/pinn/compile_config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from degann.networks.topology.base_compile_configs import SingleNetworkCompileParams
+from degann.networks.topology.pinn.virtual_loss import VirtualLoss
+
+
+
+@dataclass
+class PINNCompileParams(SingleNetworkCompileParams):
+    """
+    Compile parameters for a physics-informed neural network topology.
+    """
+    virtual_functions: List[VirtualLoss] = field(default_factory=list)
