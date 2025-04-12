1996985db29c917f6d18fef4294013e146416688
Daniil Kostromitsky <DaniilK004@mail.ru>
2025-04-12 18:50:48 +0300

    feat(pinn/topology_config): add PINN topology params

---
+++ degann/networks/topology/pinn/topology_config.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Optional, List
+
+from degann.networks.topology.base import BaseNetworkMixin
+from degann.networks.topology.base_topology_configs import SingleNetworkParams
+from degann.networks.topology.densenet.tf_densenet import TensorflowDenseNet
+from degann.networks.topology.pinn.virtual_loss import VirtualLoss
+
+
+@dataclass
+class PINNParams(SingleNetworkParams):
+    """
+    Parameters for a physics-informed neural network topology.
+    """
+
+    def __post_init__(self, metadata: Optional[dict] = None):
+        super().__post_init__(metadata)
+
+        self.net_type = "PINN"
+    
