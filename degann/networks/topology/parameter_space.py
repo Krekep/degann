d2420f5d9b716f07ea9a1337c33bfe5827d9646a
Sergey <sergey.cherepanov.06@inbox.ru>
2025-11-10 18:50:52 +0300

    Developed a class that supports the creation of parameter spaces for dense neural networks, added methods for training the model, and modified the grid search algorithm to work with objects of parameter space classes.

---
+++ degann/networks/topology/parameter_space.py
@@ -0,0 +1,130 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+from itertools import product
+from degann.search_algorithms.nn_code import alph_n_full, alphabet_activations, decode, encode
+from degann.search_algorithms.utils import update_random_generator, log_to_file
+# from .tf_densenet import TensorflowDenseNet
+from degann.networks.imodel import IModel
+
+
+class ParameterSpace(ABC):
+    """
+    Abstract class for parameter space of different neural network architectures.
+    """
+
+    @abstractmethod
+    def _create_parameter_space(self) -> Dict[Any]:
+        """
+        Abstract method for creating parameter space dict.
+        """
+        pass
+
+
+class DenseNetParameterSpace(ParameterSpace):
+    """
+    Parameter space for DenseNet.
+    """
+
+    def __init__(
+            self,
+            optimizers: list[str],
+            loss: list[str],
+            min_epoch: int = 100,
+            max_epoch: int = 700,
+            epoch_step: int = 50,
+            nn_min_length: int = 1,
+            nn_max_length: int = 6,
+            nn_alphabet: list[str] = [
+                "".join(elem) for elem in product(alph_n_full, alphabet_activations)
+            ],
+            alphabet_block_size: int = 1,
+            alphabet_offset: int = 8,
+    ):
+        self.optimizers = optimizers
+        self.loss = loss
+        self.min_epoch = min_epoch
+        self.max_epoch = max_epoch
+        self.epoch_step = epoch_step
+        self.nn_min_length = nn_min_length
+        self.nn_max_length = nn_max_length
+        self.nn_alphabet = nn_alphabet
+        self.alphabet_block_size = alphabet_block_size
+        self.alphabet_offset = alphabet_offset
+
+        self.configs = self._create_parameter_space()
+
+    def _create_parameter_space(self) -> list:
+        configs = []
+        for i in range(self.nn_min_length, self.nn_max_length + 1):
+            codes = product(self.nn_alphabet, repeat=i)
+            for elem in codes:
+                code = "".join(elem)
+                for epoch in range(self.min_epoch, self.max_epoch + 1, self.epoch_step):
+                    for opt in self.optimizers:
+                        for loss_func in self.loss:
+                            b, a = decode(code, block_size=self.alphabet_block_size, offset=self.alphabet_offset)
+                            config = {
+                                "block_size": b,
+                                "activation_func": a,
+                                "optimizer": opt,
+                                "loss_func": loss_func,
+                                "num_epoch": epoch
+                            }
+                            configs.append(config)
+        return configs
+
+    def get_configs(self) -> list:
+        return self.configs
+
+    @staticmethod
+    def train(
+            config: Dict[Any],
+            input_size: int,
+            output_size: int,
+            data: tuple,
+            repeat: int = 1,
+            update_gen_cycle: int = 0,
+            val_data: tuple = None,
+            logging: bool = False,
+            file_name: str = "",
+            callbacks: list = None,
+    ) -> tuple[float, float, dict]:
+        best_net = None
+        best_loss = 1e6
+        best_val_loss = 1e6
+        for i in range(repeat):
+            update_random_generator(i, cycle_size=update_gen_cycle)
+            history = dict()
+            nn = IModel(
+                input_size=input_size,
+                block_size=config["block_size"],
+                output_size=output_size,
+                activation_func=config["activation_func"],
+            )
+            nn.compile(optimizer=config["optimizer"], loss_func=config["loss_func"])
+            temp_his = nn.train(
+                data[0], data[1], epochs=config["num_epoch"], verbose=0, callbacks=callbacks
+            )
+
+            history["shapes"] = [nn.get_shape]
+            history["activations"] = [config["activation_func"]]
+            history["code"] = [encode(nn)]
+            history["epoch"] = [config["num_epoch"]]
+            history["optimizer"] = [config["optimizer"]]
+            history["loss function"] = [config["loss_func"]]
+            history["loss"] = [temp_his.history["loss"][-1]]
+            history["validation loss"] = (
+                [nn.evaluate(val_data[0], val_data[1], verbose=0, return_dict=True)["loss"]]
+                if val_data is not None
+                else [None]
+            )
+            history["train_time"] = [nn.network.trained_time["train_time"]]
+
+            if logging:
+                fn = f"{file_name}_{len(data[0])}_{config['num_epoch']}_{config['loss_func']}_{config['optimizer']}"
+                log_to_file(history, fn)
+            if history["loss"][0] < best_loss:
+                best_loss = history["loss"][0]
+                best_val_loss = history["validation loss"][0]
+                best_net = nn.to_dict()
+        return best_loss, best_val_loss, best_net
--- degann/search_algorithms/grid_search.py
+++ degann/search_algorithms/grid_search.py
@@ -1,198 +1,44 @@
 from datetime import datetime
-from typing import Optional, Tuple
-
-import numpy as np
-import tensorflow as tf
-
+from typing import Tuple
 from degann.networks.callbacks import MeasureTrainTime
-from degann.networks import imodel
-from degann.networks.topology.tuning_utils import generate_all_configurations
-from .search_algorithms_parameters import GridSearchParameters
-from .utils import update_random_generator, log_to_file, SearchHistory, log_search_step
-
-
-def grid_search_step(
-    model_cfg: imodel.BaseTopologyParams,
-    compile_cfg: imodel.BaseCompileParams,
-    num_epoch: int,
-    data: tuple[np.ndarray, np.ndarray],
-    repeat: int = 1,
-    val_data: Optional[tuple[np.ndarray, np.ndarray]] = None,
-    update_gen_cycle: int = 0,
-    logging: bool = False,
-    file_name: str = "",
-    callbacks: Optional[list] = None,
-    eval_metric: str = "root_mean_squared_error",
-):
-    """
-    This function is a step of the exhaustive search algorithm.
-    In this function, the passed neural network is trained (possibly several times).
-
-    Parameters
-    ----------
-    model_cfg: BaseTopologyParams
-        Config for model to train
-    compile_cfg: BaseCompileParams
-        Config for model compilation
-    num_epoch: int
-        Number of training epochs
-    data: tuple
-        Dataset
-    repeat: int
-        How many times will be repeated this step
-    val_data: Optional[tuple[np.ndarray, np.ndarray]]
-        Validation dataset
-    logging: bool
-        Logging search process to file
-    file_name: str
-        Path to file for logging
-    eval_metric: str
-        Metric used for model evaluation
-
-    Returns
-    -------
-    search_results: tuple[float, int, str, str, dict]
-        Results of the algorithm are described by these parameters
-
-        best_metric_value: float
-            The value of the metric during training of the best neural network]
-        best_val_metric_value: Optional[float]
-            The corresponding validation metric value (if validation data is provided).
-        best_net: dict
-            Best neural network presented as a dictionary
-    """
-    best_net = None
-    best_metric_value = 1e6
-    best_val_metric_value: Optional[float] = 1e6
-    for i in range(repeat):
-        update_random_generator(i, cycle_size=update_gen_cycle)
-        history = SearchHistory()
-
-        nn = imodel.IModel(model_cfg)
-        compile_cfg.add_eval_metric(eval_metric)
-        nn.compile(compile_cfg)
-
-        temp_his = nn.train(
-            data[0], data[1], epochs=num_epoch, verbose=0, callbacks=callbacks
-        )
-
-        loss_names = ("loss",) if model_cfg.net_type != "GAN" else ("g_loss", "d_loss")
-        curr_loss = [temp_his.history[name][-1] for name in loss_names]
-        curr_metric_value = temp_his.history[eval_metric][-1]
-
-        if val_data is not None:
-            val_history = nn.evaluate(
-                val_data[0], val_data[1], verbose=0, return_dict=True
-            )
-            eval_loss = [val_history[name] for name in loss_names]
-            val_metric_value = val_history[eval_metric]
-        else:
-            eval_loss = None
-            val_metric_value = None
-
-        if logging:
-            str_losses = "_".join(
-                [
-                    loss.name if isinstance(loss, tf.keras.Loss) else loss
-                    for loss_list in compile_cfg.get_losses()
-                    for loss in loss_list
-                ]
-            )
-
-            str_optimizers = "_".join(
-                [
-                    # Optimizer class doesn't have `name` attribute
-                    type(opt).__name__ if isinstance(opt, tf.keras.Optimizer) else opt
-                    for opt in compile_cfg.get_optimizers()
-                ]
-            )
-
-            fn = f"{file_name}_{len(data[0])}_{num_epoch}_{str_losses}_{str_optimizers}"
-            log_search_step(
-                model=nn,
-                activations=nn.get_activations,
-                epoch=num_epoch,
-                optimizer=str_optimizers,
-                loss_function=str_losses,
-                loss=curr_loss,
-                validation_loss=eval_loss,
-                metric_value=curr_metric_value,
-                validation_metric_value=val_metric_value,
-                file_name=fn,
-            )
-            log_to_file(history.__dict__, fn)
-
-        if curr_metric_value < best_metric_value:
-            best_metric_value = curr_metric_value
-            best_val_metric_value = val_metric_value
-            best_net = nn.to_dict()
-
-    return (best_metric_value, best_val_metric_value, best_net)
+from degann.networks.topology.parameter_space import DenseNetParameterSpace
 
 
 def grid_search(
-    parameters: GridSearchParameters,
-    verbose: bool = False,
+        input_size: int,
+        output_size: int,
+        data: tuple,
+        params: DenseNetParameterSpace,
+        val_data: tuple = None,
+        logging=False,
+        file_name: str = "",
+        verbose=False,
 ) -> Tuple[float, int, str, str, dict]:
-    """
-    An algorithm for exhaustively enumerating a given set of parameters
-    with training a neural network for each configuration of parameters
-    and selecting the best one.
-
-    Parameters
-    ----------
-    parameters: GridSearchParameters
-        Search algorithm parameters
-    verbose: bool
-        Print additional information to console during the searching
-
-    Returns
-    -------
-    search_results: tuple[float, int, str, str, dict]
-        Results of the algorithm are described by these parameters
-
-        best_metric_value: float
-            The value of the metric during training of the best neural network
-        best_epoch: int
-            Number of training epochs for the best neural network
-        best_loss_func: list[list[str]]
-            Name of the loss function of the best neural network
-        best_opt: str
-            Name of the optimizer of the best neural network
-        best_net: dict
-            Best neural network presented as a dictionary
-    """
     best_net: dict = dict()
-    best_metric_value: float = 1e6
+    best_loss: float = 1e6
     best_epoch: int = 0
     best_loss_func: str = ""
     best_opt: str = ""
     time_viewer = MeasureTrainTime()
+    configs = params.get_configs()
+    for config in configs:
+        if verbose:
+            print(len(config["block_size"]), datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+        curr_loss, curr_val_loss, curr_nn = params.train(
+            config=config,
+            input_size=input_size,
+            output_size=output_size,
+            data=data,
+            val_data=val_data,
+            logging=logging,
+            file_name=file_name,
+            callbacks=[time_viewer],
+        )
 
-    for model_cfg in generate_all_configurations(parameters.model_cfg):
-        for compile_cfg in generate_all_configurations(parameters.compile_cfg):
-            for epoch in range(
-                parameters.min_epoch, parameters.max_epoch + 1, parameters.epoch_step
-            ):
-                (
-                    curr_metric_value,
-                    curr_val_metric_value,
-                    curr_nn,
-                ) = grid_search_step(
-                    model_cfg=model_cfg,
-                    compile_cfg=compile_cfg,
-                    num_epoch=epoch,
-                    data=parameters.data,
-                    val_data=parameters.val_data,
-                    callbacks=[time_viewer],
-                    logging=parameters.logging,
-                    file_name=parameters.file_name,
-                    eval_metric=parameters.eval_metric,
-                )
-                if best_metric_value > curr_metric_value:
-                    best_net = curr_nn
-                    best_metric_value = curr_metric_value
-                    best_epoch = epoch
-                    best_loss_func = compile_cfg.get_losses()
-                    best_opt = compile_cfg.get_optimizers()
-    return best_metric_value, best_epoch, best_loss_func, best_opt, best_net
+        if best_loss > curr_loss:
+            best_net = curr_nn
+            best_loss = curr_loss
+            best_epoch = config["num_epoch"]
+            best_loss_func = config["loss_func"]
+            best_opt = config["optimizer"]
+    return best_loss, best_epoch, best_loss_func, best_opt, best_net
