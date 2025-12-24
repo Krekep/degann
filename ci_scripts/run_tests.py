f46be696b553e13ceaee36eceadb6b6a7fa78c3b
Sergey <sergey.cherepanov.06@inbox.ru>
2025-12-24 20:16:29 +0300

    pre-commit issue

--- ci_scripts/run_tests.py
+++ ci_scripts/run_tests.py
@@ -5,12 +5,9 @@ import shared
 
 def main():
     shared.configure_python_path()
-    subprocess.check_call([
-        sys.executable,
-        "-m", "pytest",
-        "-vv", "-s",
-        str(shared.TESTS)
-    ])
+    subprocess.check_call(
+        [sys.executable, "-m", "pytest", "-vv", "-s", str(shared.TESTS)]
+    )
 
 
 if __name__ == "__main__":
